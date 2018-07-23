22f863f879be1bd37ae31dc24ec3c44b52cac75e
jinyao <jinyao@deepglint.com>
2018-07-23 19:30:38 +0800

    multi thread support

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include<stdio.h>
 #include<unistd.h>
+#include <string>
+#include <iostream>
+#include <thread>
+#include <future>
+#include <vector>
+#include <tbb/pipeline.h>
+#include <tbb/concurrent_queue.h>
+
+using namespace std;
 
 #ifdef __cplusplus
 extern "C"
@@ -14,8 +23,23 @@ extern "C"
 }
 #endif
 
-const char *in_filename="rtsp://admin:admin@192.168.19.101:554/cam/realmonitor?channel=1&subtype=0";
-const char *out_filename="receive.flv";
+#define MAX_THREAD 30
+
+
+#define MACRO_CHECK_NULL(var) \
+        if(var == NULL) \
+        {                   \
+            printf("%s, %d, %s",__FILE__, __LINE__, __FUNCTION__); \
+            return -1; \
+        }
+
+
+#define MACRO_CHECK_RETURN(var) \
+        if(var < 0) \
+        {                   \
+            printf("%s, %d, %s",__FILE__, __LINE__, __FUNCTION__); \
+            return -1; \
+        }
 
 const char option_mean_key[]="rtsp_transport";
 const char option_mean_value[]="tcp";
@@ -23,53 +47,57 @@ const char option_mean_value[]="tcp";
 const char option_delay_key[]="max_delay";
 const char option_delay_value[]="5000000";
 
-
-int main(int argc,char **argv)
+int TimeOutCallBack(void* ctx)
 {
-    AVOutputFormat *ofmt = NULL;
-    AVFormatContext *ifmt_ctx = NULL;
-    AVFormatContext *ofmt_ctx = NULL;
-    AVCodecContext *in_codec_ctx = NULL;
-    AVStream *in_stream;
-    AVCodec *in_codec;
-    AVStream *out_stream;
-    AVCodec *out_codec;
-    AVCodecContext *out_codec_ctx;
+    auto p = (time_t*)ctx;
+    return (time(NULL)-*p) >= 5 ? 1 :0;
+}
 
-
+int RtspDecodePipeline(string& rtsp_src)
+{
     AVPacket pkt;
+    AVCodec *in_codec = NULL;
+    AVStream *in_stream = NULL;
+    AVDictionary *avdic = NULL;
+    AVCodecContext *in_codec_ctx = NULL;
+
     int ret = 0;
     int i = 0;
     int video_index = -1;
     int frame_index = 0;
+    time_t StartTime;
+
+    printf("thread id = %x running \n", this_thread::get_id());
 
     AVFrame *pFrame = av_frame_alloc();
-    AVFrame *pFrameRGB = av_frame_alloc();
+    MACRO_CHECK_NULL(pFrame);
 
-    if(pFrameRGB==NULL)
-    {
-        return -1;
-    }
+    AVFrame *pFrameRGB = av_frame_alloc();
+    MACRO_CHECK_NULL(pFrameRGB);
+
+    AVFormatContext *ifmt_ctx = avformat_alloc_context();
+    MACRO_CHECK_NULL(ifmt_ctx);
 
     av_register_all();
 
-    avformat_network_init();
+    ret = avformat_network_init();
+    MACRO_CHECK_RETURN(ret);
 
-    AVDictionary *avdic=NULL;
-    av_dict_set(&avdic, option_mean_key, option_mean_value, 0);
-    av_dict_set(&avdic, option_delay_key, option_delay_value, 0);
+    ret = av_dict_set(&avdic, option_mean_key, option_mean_value, 0);
+    MACRO_CHECK_RETURN(ret);
 
-    if((ret = avformat_open_input(&ifmt_ctx, in_filename, 0, &avdic)) < 0)
-    {
-        printf("Could not open input file.\n");
-        goto end;
-    }
+    ret = av_dict_set(&avdic, option_delay_key, option_delay_value, 0);
+    MACRO_CHECK_RETURN(ret);
 
-    if((ret = avformat_find_stream_info(ifmt_ctx, 0)) < 0)
-    {
-        printf("Failed to retrieve input stream information\n");
-        goto end;
-    }
+    StartTime = time(NULL);
+    ifmt_ctx->interrupt_callback.callback = TimeOutCallBack;
+    ifmt_ctx->interrupt_callback.opaque = &StartTime;
+
+    ret = avformat_open_input(&ifmt_ctx, rtsp_src.c_str(), 0, &avdic);
+    MACRO_CHECK_RETURN(ret);
+
+    ret = avformat_find_stream_info(ifmt_ctx, 0);
+    MACRO_CHECK_RETURN(ret);
 
     for(i = 0; i < ifmt_ctx->nb_streams; i++)
     {
@@ -79,19 +107,6 @@ int main(int argc,char **argv)
             break;
         }
     }
-
-    av_dump_format(ifmt_ctx, 0, in_filename, 0);
-
-    avformat_alloc_output_context2(&ofmt_ctx, NULL, NULL, out_filename);
-
-    if(!ofmt_ctx)
-    {
-        printf("Could not create output context\n");
-        ret = AVERROR_UNKNOWN;
-        goto end;
-    }
-
-    ofmt = ofmt_ctx->oformat;
 
     in_stream = ifmt_ctx->streams[video_index];
 
@@ -104,23 +119,20 @@ int main(int argc,char **argv)
         in_codec_ctx->flags|=CODEC_FLAG_TRUNCATED;
     }
 
-    if(avcodec_open2(in_codec_ctx, in_codec,NULL) != 0)
-    {
-        printf("decode errot \n");
-        return -1;
-    }
+    ret = avcodec_open2(in_codec_ctx, in_codec,NULL);
+    MACRO_CHECK_RETURN(ret);
+
+    StartTime = time(NULL);
 
     while(!av_read_frame(ifmt_ctx, &pkt))
     {
         if(pkt.stream_index == video_index && pkt.flags == AV_PKT_FLAG_KEY)  //video frame
         {
-            printf("Receive %8d video frames from input URL\n", frame_index);
             avcodec_send_packet(in_codec_ctx, &pkt);
             avcodec_receive_frame(in_codec_ctx, pFrame);
 
             if (pFrame->key_frame == 1)
             {
-                printf("key frame = %d", 1);
                 int picSize = in_codec_ctx->height * in_codec_ctx->width;
                 int newSize = picSize * 1.5;
 
@@ -128,7 +140,7 @@ int main(int argc,char **argv)
 
                 int height = in_codec_ctx->height;
                 int width = in_codec_ctx->width;
-/*
+
                 int a=0;
                 for (i=0; i<height; i++)
                 {
@@ -145,7 +157,7 @@ int main(int argc,char **argv)
                     memcpy(buf+a,pFrame->data[2] + i * pFrame->linesize[2], width/2);
                     a+=width/2;
                 }
-                */
+                printf("%d \n", frame_index);
 /*
                 FILE *pFile;
                 char szFilename[32];
@@ -164,29 +176,41 @@ int main(int argc,char **argv)
                 fclose(pFile);
 */
                 frame_index++;
-                usleep(500000);
             }
         }
         av_packet_unref(&pkt);
+        StartTime = time(NULL);
     }
-
-    av_write_trailer(ofmt_ctx);
-
-    end:
+    printf("exit thread \n");
     av_dict_free(&avdic);
     avformat_close_input(&ifmt_ctx);
     av_frame_free(&pFrame);
     av_frame_free(&pFrameRGB);
+    avformat_free_context(ifmt_ctx);
 
-    if(ofmt_ctx && !(ofmt->flags & AVFMT_NOFILE))
+    return 0;
+}
+
+
+int main(int argc,char **argv)
+{
+    std::string file_name_pool[MAX_THREAD];
+
+    for(int i = 0; i < MAX_THREAD; ++i)
     {
-        avio_close(ofmt_ctx->pb);
+        file_name_pool[i] = "rtsp://192.168.2.123/live/t1030";
     }
-    avformat_free_context(ofmt_ctx);
-    if(ret < 0 && ret != AVERROR_EOF)
+
+    std::thread thread_pool[MAX_THREAD];
+    for(int i =0; i< MAX_THREAD; ++i)
     {
-        printf("Error occured.\n");
-        return -1;
+        thread_pool[i] = thread(RtspDecodePipeline, std::ref(file_name_pool[i]));
     }
+    for(auto &t : thread_pool)
+    {
+        t.join();
+    }
+
+    std::cout << "all threads joined" << std::endl;
     return 0;
 }
